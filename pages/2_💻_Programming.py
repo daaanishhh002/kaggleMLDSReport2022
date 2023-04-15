2d4afc419c9c0c57b628b5c45f88e201292033f9
Danish Ahmed <92080471+daaanishhh4218@users.noreply.github.com>
2023-04-15 17:37:19 +0530

    Update 2_💻_Programming.py

--- pages/2_💻_Programming.py
+++ pages/2_💻_Programming.py
@@ -9,8 +9,8 @@ st.set_page_config(page_title="Kaggle Machine Learning & Data Science Report 202
 plt.style.use('ggplot')
 sns.set_theme(font = 'Georgia', palette = 'deep')
 
-path = "C:\\Users\\dzuz1\\Desktop\\Python\\datasets\\kaggle_survey_2022_responses.csv"
-df = pd.read_csv(path)
+url = "https://raw.githubusercontent.com/daaanishhh4218/kaggleMLDSReport2022/main/kaggle_survey_2022_responses.csv"
+df = pd.read_csv(url)
 
 df = df.rename(columns = {'Duration (in seconds)': 'Q1'})
 
