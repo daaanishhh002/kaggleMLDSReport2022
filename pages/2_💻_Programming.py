213c10b0fff020904a736780e27b306e5ce46f92
Danish Ahmed <92080471+daaanishhh4218@users.noreply.github.com>
2023-04-16 11:26:13 +0530

    Update 2_💻_Programming.py

--- pages/2_💻_Programming.py
+++ pages/2_💻_Programming.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-st.set_page_config(page_title="Kaggle Machine Learning & Data Science Report 2022", layout="wide")
+st.set_page_config(page_title="Kaggle Machine Learning & Data Science Report 2022", layout="wide", page_icon='👨‍💻')
 
 plt.style.use('ggplot')
-sns.set_theme(palette = 'deep')
+sns.set_theme(font = 'Georgia', palette = 'deep')
 
-url = "https://raw.githubusercontent.com/daaanishhh4218/kaggleMLDSReport2022/main/kaggle_survey_2022_responses.csv"
+url = "https://raw.githubusercontent.com/daaanishhh4218/kaggleMLDSReport2022/main/kaggle_survey_2022_responses.csv" 
 df = pd.read_csv(url)
 
 df = df.rename(columns = {'Duration (in seconds)': 'Q1'})
@@ -56,7 +56,7 @@ def to_show(sname, title):
     
     return fig
 
-st.header('[Some Infographics Related To Programming]')
+st.header('Some Infographics Related To Programming')
 st.markdown('---')
 
 
@@ -75,6 +75,8 @@ with st.container():
     with col2:
         st.pyplot(fig)
 
+st.write('\n')
+
 fig, ax = plt.subplots()
 lang = to_transform('Q12_1', 'Q12_15', 'lang_df', 'lang', 'Language')
 sns.barplot(data = lang, y = 'Language', x = 'Count', palette = 'deep')
@@ -87,7 +89,9 @@ with st.container():
         st.pyplot(fig)
     with col2:
         st.write('Caption for second chart')
-        
+
+st.write('\n')
+
 fig, ax = plt.subplots()
 visual = to_transform('Q15_1', 'Q15_15', 'v_df', 'visual', 'Visualisation Library')
 visual['Visualisation Library'] = visual['Visualisation Library'].drop(4)
@@ -100,7 +104,9 @@ with st.container():
         st.pyplot(fig)
     with col1:
         st.write('Caption for second chart')
-        
+
+st.write('\n')
+
 fig, ax = plt.subplots()
 df['Q16'] = df['Q16'].str.replace('machine', 'machine\n')
 sns.countplot(data = df, y = 'Q16', palette = 'deep')
@@ -136,6 +142,8 @@ with st.container():
     with col1:
         st.write('Caption for second chart')
 
+st.write('\n')
+
 fig, ax = plt.subplots()
 ml_algo = to_transform('Q18_1', 'Q18_14', 'ml_df', 'ml_algo', 'ML Algorithm')
 ml_algo['ML Algorithm'] = ml_algo['ML Algorithm'].str.replace('(', '\n(')
@@ -150,6 +158,7 @@ with st.container():
     with col2:
         st.write('Caption for second chart')
 
+st.write('\n')
 
 fig, ax = plt.subplots(1, 1, figsize = (25, 15))
 cv = to_transform('Q19_1', 'Q19_8', 'cv_df', 'cv', 'CV Algorithm')
@@ -165,6 +174,8 @@ with st.container():
     with col1:
         st.write('Caption for second chart')
 
+st.write('\n')
+
 fig, ax = plt.subplots()
 nlp = to_transform('Q20_1', 'Q20_6', 'nlp_df', 'nlp', 'NLP Algorithm')
 nlp['NLP Algorithm'] = nlp['NLP Algorithm'].str.replace('(', '\n(')
@@ -179,6 +190,8 @@ with st.container():
     with col2:
         st.write('Caption for second chart')
 
+st.write('\n')
+
 fig, ax = plt.subplots()
 cloud = to_transform('Q31_1', 'Q31_12', 'cloud_df', 'cloud', 'Cloud Service')
 cloud['Cloud Service'] = cloud['Cloud Service'].drop([3, 6])
@@ -192,6 +205,8 @@ with st.container():
     with col1:
         st.write('Caption for second chart')
 
+st.write('\n')
+
 fig, ax = plt.subplots()
 db = to_transform('Q35_1', 'Q35_16', 'db_df', 'db', 'Database')
 db['Database'] = db['Database'].drop([5, 14])
@@ -204,7 +219,9 @@ with st.container():
         st.pyplot(fig)
     with col2:
         st.write('Caption for second chart')
-        
+
+st.write('\n')
+
 fig, ax = plt.subplots()
 bi = to_transform('Q36_1', 'Q36_15', 'bi_df', 'bi', 'BI Tool')
 bi['BI Tool'] = bi['BI Tool'].drop([0, 6])
